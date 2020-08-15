854a43643d9fe722c2f513a56046db10e6ad82a7
Dan Simpson <DSimpson@habitat.org>
2020-08-14 22:07:40 -0400

    added ganado

--- doodles/ganado.js
+++ doodles/ganado.js
@@ -2,11 +2,14 @@ import { tweak } from 'https://cdn.jsdelivr.net/gh/jonahs99/doodle/lib.js'
 
 export function config() {
 	return tweak.label('Ganado:', {
-		iterations: tweak.integer(36),
-		decay: 0.9,
-		length: 180,
-		angle: 5,
+		iterations: tweak.integer(1),
+    // sides: 2,
+		sides: [{
+			angle: 5,
+			length: 180,
+		}]
 	})
+
 }
 
 export function setup({ config, ctx, canvas }) {
@@ -18,32 +21,57 @@ export function setup({ config, ctx, canvas }) {
 	ctx.translate(canvas.width / 2, canvas.height / 2)
 	ctx.rotate(-Math.PI / 2)
 
-	ctx.lineWidth = 6;
+	ctx.lineWidth = 6
 
-	tree({ config, ctx })
+	ganado({ config, ctx })
 }
 
-function tree({ config, ctx }, n = config.iterations) {
-	if (n <= 0) return
+function ganado({ config, ctx }, n = config.iterations) {
+  // Create array of vectors (the 'model')
+	// Q for Jonah: can tweak learn to assign variable length arrays/hashes?
+  var model = [
+		{ angle: config.angle_1, length: config.length_1},
+		{ angle: config.angle_2, length: config.length_2}
+	]
+  var figure = []
 
-	const l = config.length
-	const t = config.angle
-	const midpt = { x: l, y: 0 }
-	const endpt = { x: midpt.x + l * Math.cos(t), y: l * Math.sin(t) }
+	//Array(n).forEach((iteration) => {
+		model.forEach((model_side) => {
+			console.log(model_side)
+		})
+  //})
 
-	ctx.save()
-
+  ctx.save()
 	ctx.beginPath()
-	ctx.moveTo(0, 0)
-	//ctx.quadraticCurveTo(midpt.x, midpt.y, endpt.x, endpt.y)
-	ctx.lineTo(endpt.x, endpt.y)
-	ctx.stroke()
+	ctx.moveTo(0,0)
+	ctx.lineTo (200, 200)
+  ctx.stroke()
 
-	ctx.translate(endpt.x, endpt.y)
-	ctx.rotate(t)
-	ctx.scale(config.decay, config.decay)
+  //drawVector(beginpt)//, {vector.ang, vector.mag})
 
-	tree({ config, ctx }, n - 1)
-
+  //ganado({ config, ctx }, n - 1)
+//
 	ctx.restore()
+ }
+// function iterate_sides(angle, ii = config.iterations, ss = config.sides) {
+// 	rng(1..ss).forEach((s) => {
+//     console.log(s)
+// 	}
+// }
+
+// function drawVector({ x, y }, { angle, length }) {
+// 	var line = {
+// 		beginpt: { x: x, y: y },
+// 		endpt: {x: x + length * Math.cos(angle), y: y + length * Math.sin(angle)}
+// 	}
+// 		ctx.beginPath()
+// 		ctx.moveTo(beginpt.x, beginpt.y)
+// 		ctx.lineTo(endpt.x, endpt.y)
+// //		ctx.stroke()
+//  	return line
+// }
+
+//function rng: return an array of integers from start to end
+function rng(start, end, step = 1) {
+  return Array(Math.ceil((end - start) / step) + 1).fill(start).map((start, idx) => start + idx * step)
 }
