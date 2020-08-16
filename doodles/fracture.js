ce014158f5e22ada41c072810700f773d1941ac5
Jonah Simpson <jonahs99@gmail.com>
2020-08-16 16:44:23 -0400

    Fracture: substantial speed gain with spatialhash

--- doodles/fracture.js
+++ doodles/fracture.js
@@ -3,12 +3,13 @@ import { tweak, Prando } from '../lib.js'
 export default tweak.doodle(() => tweak.label('FRACTURE (from Inconvergent):', {
     randomSeed: tweak.integer(3),
     radius: 400,
-    iterations: tweak.integer(6000),
+    iterations: tweak.integer(20000),
     speed: 40,
     wander: 0.3,
     angle: 0.1,
     weight: 4,
-    decay: 0.84,
+    decay: 0.75,
+    sourceThresh: tweak.number(0.2, 0.01, 0, 1),
 }), {
     *setup({ config, canvas, ctx }) {
         canvas.style.background = '#eee'
@@ -20,32 +21,43 @@ export default tweak.doodle(() => tweak.label('FRACTURE (from Inconvergent):', {
 
         const lines = []
         const longLines = []
+        const grid = {}
 
         let dir = rng.next(0, 2*Math.PI)
         let pen = polar(dir, -config.radius)
         let weight = config.weight
         for (let i = 0; i < config.iterations; i++) {
+            const penHash = hash(pen, config.speed)
+            const nbrs = nbr(pen, config.speed)
+
             dir += rng.next(-config.wander, config.wander)
             let d = polar(dir, config.speed)//exp(config.speed, rng))
-            const t = Math.min(1, ...lines.map(({seg}) => hit([pen, d], seg)).filter(t => !isNaN(t) && t > 0)) 
+            const targets = nbrs.flatMap(h => grid[h] ?? [])
+            const t = Math.min(1, ...targets.map(({seg}) => hit([pen, d], seg)).filter(t => !isNaN(t) && t > 0)) 
             if (t < 1) {
                 d = scale(d, t)
             }
 
             const seg = [pen, d]
-            lines.push({ seg, weight })
-            // if (mag(d) > config.speed / 2) longLines.push([pen, d])
+            const line = { seg, weight }
+            lines.push(line)
+            if (t > config.sourceThresh) longLines.push(line)
+
+            // Add line to the spatial hash
+            grid[penHash] = grid[penHash] ?? []
+            grid[penHash].push(line)
+
             pen = add(pen, d)
 
-            if (t < 1 || mag(pen) > config.radius) {
-                const line = rng.nextArrayItem(lines)// lines[rng.nextInt(0, lines.length-1)]
+            if (t < 1 || mag2(pen) > config.radius*config.radius) {
+                const line = rng.nextArrayItem(longLines)// lines[rng.nextInt(0, lines.length-1)]
                 pen = add(line.seg[0], scale(line.seg[1], rng.next()))
                 dir = heading(line.seg[1]) + Math.PI / 2 + (rng.nextInt(0, 1) * Math.PI)
                     + rng.next(-config.angle, config.angle)
                 weight = line.weight * config.decay
             }
 
-            if (i % 100 === 99 || i === config.iterations - 1) {
+            if (i % 1000 === 999 || i === config.iterations - 1) {
                 ctx.setTransform(1, 0, 0, 1, 0, 0)
                 ctx.clearRect(0, 0, canvas.width, canvas.height)
                 ctx.translate(canvas.width / 2, canvas.height / 2)
@@ -64,6 +76,21 @@ export default tweak.doodle(() => tweak.label('FRACTURE (from Inconvergent):', {
     },
 })
 
+function hash({x, y}, size) {
+    return `${Math.floor(x/size)}|${Math.floor(y/size)}`
+}
+function nbr({x, y}, size) {
+    const i = Math.floor(x / size)
+    const j = Math.floor(y / size)
+    const cells = []
+    for (const ni of [i-1,i,i+1]) {
+        for (const nj of [j-1,j,j+1]) {
+            cells.push(`${ni}|${nj}`)
+        }
+    }
+    return cells
+}
+
 function exp(mean, rng) {
     return -mean * Math.log(1 - rng.next())
 }
@@ -73,7 +100,7 @@ const polar = (t, r=1) => vec(Math.cos(t) * r, Math.sin(t) * r)
 const zero = {x: 0, y: 0}
 const add = (...vecs) => vecs.reduce((a, b) => vec(a.x + b.x, a.y + b.y))
 const scale = ({x, y}, a) => vec(x * a, y * a)
-const mag = ({x, y}) => Math.sqrt(x*x + y*y)
+const mag2 = ({x, y}) => x*x + y*y
 const heading = ({x, y}) => Math.atan2(y, x)
 
 const hit = (ray, seg) => {
