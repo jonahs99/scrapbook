ffbdc78ab04ffe3959e736483f1b2a6ea9cbd792
Dan Simpson <DSimpson@habitat.org>
2020-08-16 13:09:02 -0400

    make it work

--- doodles/ganado.js
+++ doodles/ganado.js
@@ -1,76 +1,146 @@
-import { tweak } from 'https://cdn.jsdelivr.net/gh/jonahs99/doodle/lib.js'
+import { tweak } from '../lib.js'
 
 export function config() {
 	return tweak.label('Ganado:', {
+		test: false,
 		iterations: tweak.integer(1),
-    // sides: 2,
-		model: [{
-			angle: 5,
-			length: 180,
-		}]
+	  model: [{
+				angle: tweak.number(60,1,0,360),
+				length: tweak.number(100,1,1,100)
+			}, {
+				angle: 300,
+				length: 100
+			}, {
+				angle: tweak.number(60,1,0,360),
+				length: tweak.number(100,1,1,100)
+			}, {
+				angle: 300,
+				length: 100
+			}
+	],
+		lineWidth: 2
 	})
-
 }
 
 export function setup({ config, ctx, canvas }) {
-	canvas.style.background = '#eee'
+	if (config.test) {
+		test(config)
+	} else {
 
-	ctx.setTransform(1, 0, 0, 1, 0, 0)
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
+		let figure = ganado(config.model, config.iterations, config)
 
-	ctx.translate(canvas.width / 2, canvas.height / 2)
-	ctx.rotate(-Math.PI / 2)
-
-	ctx.lineWidth = 6
-
-	ganado({ config, ctx })
+    drawFigure(figure, canvas, ctx)
+  }
+}
+function ganado( figure, n = config.iterations, config ) {
+// Return an array of vectors (the 'figure') that result from iterating over the the 'model'
+	const _model = deepCopy(config.model)
+	let _figure = deepCopy(figure)
+	if (n > 1) {
+		_figure = ganado(_figure, n - 1, config)
+		_model.forEach((vector, index) => {
+			_model.splice(index, 1, figureRotate(_figure,vector.angle))
+		})
+	}
+  return _model.flat()
 }
 
-function ganado({ config, ctx }, n = config.iterations) {
-  // Create array of vectors (the 'model')
-  var model = [
-		{ angle: config.angle_1, length: config.length_1},
-		{ angle: config.angle_2, length: config.length_2}
-	]
-  var figure = []
+function drawFigure(figure, canvas, ctx) {
+  figure = figurePath(figure)
+	configureContext(canvas, ctx, figureDimensions(figure))
+  drawPath(figure, ctx)
+}
 
-	//Array(n).forEach((iteration) => {
-		model.forEach((model_side) => {
-			console.log(model_side)
-		})
-  //})
-
-  ctx.save()
+function drawPath( figure, ctx ) {
 	ctx.beginPath()
 	ctx.moveTo(0,0)
-	ctx.lineTo (200, 200)
-  ctx.stroke()
+	figure.forEach(vector => {
+    ctx.lineTo(vector.endpt.x, vector.endpt.y)
+	})
+	ctx.stroke()
+}
 
-  //drawVector(beginpt)//, {vector.ang, vector.mag})
+function figurePath(figure) {
+	let beginpt = {x:0, y:0}
+	figure.forEach((segment) => {
+		let endpt = vectorEndpoint(segment, beginpt)
+		segment.beginpt = beginpt
+		segment.endpt = endpt
+		beginpt = endpt
+	})
+	return figure
+}
 
-  //ganado({ config, ctx }, n - 1)
-//
-	ctx.restore()
- }
-// function iterate_sides(angle, ii = config.iterations, ss = config.sides) {
-// 	rng(1..ss).forEach((s) => {
-//     console.log(s)
-// 	}
-// }
+function vectorEndpoint(vector, beginpt) {
+	let endpt = {
+		x: beginpt.x + vector.length * dCos(vector.angle),
+		y: beginpt.y + vector.length * dSin(vector.angle)
+	}
+	return endpt
+}
 
-// function drawVector({ x, y }, { angle, length }) {
-// 	var line = {
-// 		beginpt: { x: x, y: y },
-// 		endpt: {x: x + length * Math.cos(angle), y: y + length * Math.sin(angle)}
-// 	}
-// 		ctx.beginPath()
-// 		ctx.moveTo(beginpt.x, beginpt.y)
-// 		ctx.lineTo(endpt.x, endpt.y)
-// //		ctx.stroke()
-//  	return line
-// }
+function figureDimensions(figure) {
+	let xs = figure.map( segment => [segment.beginpt.x, segment.endpt.x])
+	let ys = figure.map( segment => [segment.beginpt.y, segment.endpt.y])
+  let dimensions = {
+		x_min:Math.min(...xs),
+		x_max:Math.max(...xs),
+		y_min:Math.min(...ys),
+		y_max:Math.max(...ys)
+	}
+  return dimensions
+}
+
+function test(config) {
+	const a = [{x:1,y:2},[{x:10,y:20},{x:100,y:200}]].flat()
+  console.log(a)
+}
+
+function figureRotate(figure, rotation) {
+	const _figure =deepCopy(figure)
+	_figure.forEach(vector => {
+		vector.angle = (vector.angle + rotation) % 360
+	})
+	return _figure
+}
+
+function configureContext(canvas, ctx, figureDimensions) {
+	const figureWidth = figureDimensions.x_max-figureDimensions.x_min
+	const figureHeight = figureDimensions.y_max-figureDimensions.y_min
+	const hPos = (figureDimensions.x_min/figureWidth)*figureDimensions.x_min
+	const vPos = figureDimensions.y_min/figureHeight
+
+	const ctxScale = Math.min(figureWidth/canvas.width,figureHeight/canvas.height)
+
+	canvas.style.background = '#eee'
+	// ctx.setTransform(1, 0, 0, 1, 0, 0)
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+	// ctx.translate(canvas.width / 2, canvas.height / 2)
+	//ctx.translate(hPos, vPos)
+	//ctx.scale(ctxScale,ctxScale)
+	ctx.lineWidth = config.lineWidth
+}
+
+function dSin(deg) {
+	return Math.sin(deg2rad(deg))
+}
+function dCos(deg) {
+	return Math.cos(deg2rad(deg))
+}
+function deg2rad(deg) {
+	return deg/180*Math.PI
+}
 
 //function rng: return an array of integers from start to end
 function rng(start, end, step = 1) {
   return Array(Math.ceil((end - start) / step) + 1).fill(start).map((start, idx) => start + idx * step)
+}
+
+function msg(debug, message=null) {
+	if (message) console.log( message + ':' )
+	console.log( deepCopy(debug) )
+}
+
+function deepCopy(val) {
+  return val && JSON.parse(JSON.stringify(val))
 }
