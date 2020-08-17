5e7da2d8a21cd49ec4d1067192cd4e4e1f78286d
Jonah Simpson <jonahs99@gmail.com>
2020-08-16 20:05:15 -0400

    Better render loop

--- doodle.js
+++ doodle.js
@@ -4,43 +4,66 @@ export function mountDoodle(doodle, canvas, configContainer) {
 	const ctx = canvas.getContext('2d')	
 	let config
 
-	const runDoodle = (nextConfig) => {
-		config = nextConfig
+	let setup
+	let setupGen
 
-		const doodleParams = () => ({ config, canvas, ctx })
-
-		// For now, call setup on every config change
-		if (doodle.setup) {
-			const setupGen = doodle.setup(doodleParams())
-			if (setupGen) {
-				const drive = () => {
-					if (!setupGen.next().done) {
-						requestAnimationFrame(drive)
-					}
-				}
-				drive()
-			}
+	const doodleParams = () => ({ config, canvas, ctx })
+	
+	const loop = () => {
+		if (setup) {
+			setupGen = setup(doodleParams())
+			setup = undefined
+		} else if (setupGen) {
+			if (setupGen.next().done) setupGen = undefined
+		} else {
+			if (doodle.draw) doodle.draw(doodleParams())
 		}
-
-		if (doodle.draw) {
-			const loop = () => {
-				if (config !== nextConfig) return;
-				doodle.draw(doodleParams())
-				requestAnimationFrame(loop)
-			}
-			loop()
-		}
+		requestAnimationFrame(loop)
 	}
+	requestAnimationFrame(loop)
+
+	const restartDoodle = (newConfig) => {
+		config = newConfig
+		setup = doodle.setup
+	}
+
+	// const runDoodle = (nextConfig) => {
+	// 	config = nextConfig
+
+
+	// 	// For now, call setup on every config change
+	// 	if (doodle.setup) {
+	// 		setupGen = doodle.setup(doodleParams())
+	// 		if (setupGen) {
+	// 			const drive = () => {
+	// 				if (config !== nextConfig) return;
+	// 				if (!setupGen.next().done) {
+	// 					requestAnimationFrame(drive)
+	// 				}
+	// 			}
+	// 			drive()
+	// 		}
+	// 	}
+
+	// 	if (doodle.draw) {
+	// 		const loop = () => {
+	// 			if (config !== nextConfig) return;
+	// 			doodle.draw(doodleParams())
+	// 			requestAnimationFrame(loop)
+	// 		}
+	// 		loop()
+	// 	}
+	// }
 
 	const resizeCanvas = () => {
 		canvas.width = canvas.parentElement.offsetWidth
 		canvas.height = canvas.parentElement.offsetHeight
 
-		if (config) runDoodle(config)
+		if (config) restartDoodle(config)
 	}
 	resizeCanvas()
 	window.addEventListener('resize', resizeCanvas)
 
-	tweak.render(tweak.field(doodle.config()), configContainer, runDoodle)
+	tweak.render(tweak.field(doodle.config()), configContainer, restartDoodle)
 }
 
