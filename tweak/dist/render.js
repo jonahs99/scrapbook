b321bdba3d0f8a069e34312aaad7edd8560ad1a3
Jonah Simpson <jonahs99@gmail.com>
2020-09-21 21:42:08 -0400

    Save to local storage

--- tweak/dist/render.js
+++ tweak/dist/render.js
@@ -1,12 +1,30 @@
 import { render as renderTemplate } from '../node_modules/lit-html/lit-html.js';
 export const render = ({ state, template, getValue }, el, onChange) => {
-    const renderState = (state) => {
-        renderTemplate(template(state, renderState), el);
+    const renderState = (newState) => {
+        renderTemplate(template(newState, renderState), el);
         if (onChange)
-            onChange(getValue(state));
+            onChange(getValue(newState));
+        setTimeout(() => setStoredState(state, newState), 0);
     };
-    renderState(state);
+    renderState(getStoredState(state) ?? state);
 };
+function getStoredState(state) {
+    const str = localStorage.getItem(JSON.stringify(storageKey(state)));
+    return str && JSON.parse(str);
+}
+function setStoredState(initialState, state) {
+    localStorage.setItem(JSON.stringify(storageKey(state)), JSON.stringify(state));
+}
+function storageKey(state) {
+    if (Array.isArray(state)) {
+        return state.map(storageKey);
+    }
+    else if (typeof state === 'object') {
+        return state && Object.fromEntries(Object.entries(state)
+            .map(([key, val]) => [key, storageKey(val)]));
+    }
+    return typeof state;
+}
 /** Creates a doodle with setup() and draw() types inferred from the config */
 export function doodle(config, doodle) {
     return { ...doodle, config };
--- tweak/src/render.ts
+++ tweak/src/render.ts
@@ -2,11 +2,31 @@ import { render as renderTemplate } from '../node_modules/lit-html/lit-html.js'
 import { Field, InferValue } from './field'
 
 export const render = <T, S>({ state, template, getValue }: Field<T, S>, el: Element | DocumentFragment, onChange?: (value: T) => VideoFacingModeEnum) => {
-	const renderState = (state: S) => {
-		renderTemplate(template(state, renderState), el)
-		if (onChange) onChange(getValue(state))
+	const renderState = (newState: S) => {
+		renderTemplate(template(newState, renderState), el)
+		if (onChange) onChange(getValue(newState))
+		setTimeout(() => setStoredState(state, newState), 0)
 	}
-	renderState(state)
+	renderState(getStoredState(state) ?? state)
+}
+
+function getStoredState(state: unknown) {
+	const str = localStorage.getItem(JSON.stringify(storageKey(state)))
+	return str && JSON.parse(str)
+}
+
+function setStoredState(initialState: unknown, state: unknown) {
+	localStorage.setItem(JSON.stringify(storageKey(state)), JSON.stringify(state))
+}
+
+function storageKey(state: unknown): unknown {
+	if (Array.isArray(state)) {
+		return state.map(storageKey)
+	} else if (typeof state === 'object') {
+		return state && Object.fromEntries(Object.entries(state)
+			.map(([key, val]) => [key, storageKey(val)]))
+	}
+	return typeof state
 }
 
 type DoodleProps<C> = {
