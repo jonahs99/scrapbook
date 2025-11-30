b7b5480c7a127769caa9f720356b6f2b11ddb1cb
jonah simpson <jonahs99@gmail.com>
2025-11-29 21:49:33 -0500

    Use relative urls

--- doodle/doodle.js
+++ doodle/doodle.js
@@ -1,81 +1,86 @@
-import * as tweak from './tweak/dist/index.js'
+import * as tweak from "./tweak/dist/index.js";
 
-import { PlotContext } from './plot/plot.js'
+import { PlotContext } from "./plot/plot.js";
 
 export async function mountDoodle(name, canvas, configContainer, initialState) {
-	const doodle = await loadDoodle(name)
-	const state = mount(doodle.default ?? doodle, canvas, configContainer, initialState)
-	return `/doodle/?doodle=${name}#config=${encodeURIComponent(JSON.stringify(state))}`
+  const doodle = await loadDoodle(name);
+  const state = mount(
+    doodle.default ?? doodle,
+    canvas,
+    configContainer,
+    initialState,
+  );
+  return `doodle/?doodle=${name}#config=${encodeURIComponent(JSON.stringify(state))}`;
 }
 
 async function loadDoodle(name) {
-	const defaultScript = 'tree'
-	try {
-		return await import(`./doodles/${name || defaultScript}.js`)
-	} catch (err) {
-		console.error(err)
-		return await import(`./doodles/${defaultScript}.js`)
-	}
+  const defaultScript = "tree";
+  try {
+    return await import(`./doodles/${name || defaultScript}.js`);
+  } catch (err) {
+    console.error(err);
+    return await import(`./doodles/${defaultScript}.js`);
+  }
 }
 
 function mount(doodle, canvas, configContainer, initialState) {
-	const ctx = canvas.getContext('2d')	
-	let config
+  const ctx = canvas.getContext("2d");
+  let config;
 
-	let setup
-	let setupGen
+  let setup;
+  let setupGen;
 
-	const doodleParams = () => ({ config, canvas, ctx })
-	
-	const loop = () => {
-		if (setup) {
-			setupGen = setup(doodleParams())
-			setup = undefined
-		} else if (setupGen) {
-			if (setupGen.next().done) setupGen = undefined
-		} else {
-			if (doodle.draw) doodle.draw(doodleParams())
-		}
-		requestAnimationFrame(loop)
-	}
-	requestAnimationFrame(loop)
+  const doodleParams = () => ({ config, canvas, ctx });
 
-	const restartDoodle = (newConfig) => {
-		config = newConfig
-		setup = doodle.setup
-	}
+  const loop = () => {
+    if (setup) {
+      setupGen = setup(doodleParams());
+      setup = undefined;
+    } else if (setupGen) {
+      if (setupGen.next().done) setupGen = undefined;
+    } else {
+      if (doodle.draw) doodle.draw(doodleParams());
+    }
+    requestAnimationFrame(loop);
+  };
+  requestAnimationFrame(loop);
 
-	const resizeCanvas = () => {
-		canvas.width = canvas.offsetWidth * devicePixelRatio
-		canvas.height = canvas.offsetHeight * devicePixelRatio
+  const restartDoodle = (newConfig) => {
+    config = newConfig;
+    setup = doodle.setup;
+  };
 
-		if (config) restartDoodle(config)
-	}
-	requestAnimationFrame(resizeCanvas)
-	window.addEventListener('resize', resizeCanvas)
+  const resizeCanvas = () => {
+    canvas.width = canvas.offsetWidth * devicePixelRatio;
+    canvas.height = canvas.offsetHeight * devicePixelRatio;
 
-	const plotDoodle = () => {
-		const params = { config, canvas, ctx: new PlotContext() }
-		const setupGen = doodle.setup(params)
-		if (setupGen) {
-			for (const _ of setupGen) { }
-		}
-		if (doodle.draw) doodle.draw(params)
-		const scale = 200 / canvas.height
-		const center = { x: canvas.width / 2, y: canvas.height / 2 }
-		console.log(params.ctx.output({ scale, center }))
-	}
-	document.querySelector('#btn-plot')?.addEventListener('click', plotDoodle)
+    if (config) restartDoodle(config);
+  };
+  requestAnimationFrame(resizeCanvas);
+  window.addEventListener("resize", resizeCanvas);
 
-	if (configContainer) {
-		const config = doodle.config()
-		config.state = initialState || config.state
-		tweak.render(tweak.field(config), configContainer, restartDoodle)
-		return config.state
-	} else {
-		const { state, getValue } = doodle.config()
-		restartDoodle(getValue(initialState || state))
-		return initialState || state
-	}
+  const plotDoodle = () => {
+    const params = { config, canvas, ctx: new PlotContext() };
+    const setupGen = doodle.setup(params);
+    if (setupGen) {
+      for (const _ of setupGen) {
+      }
+    }
+    if (doodle.draw) doodle.draw(params);
+    const scale = 200 / canvas.height;
+    const center = { x: canvas.width / 2, y: canvas.height / 2 };
+    console.log(params.ctx.output({ scale, center }));
+  };
+  document.querySelector("#btn-plot")?.addEventListener("click", plotDoodle);
+
+  if (configContainer) {
+    const config = doodle.config();
+    config.state = initialState || config.state;
+    tweak.render(tweak.field(config), configContainer, restartDoodle);
+    return config.state;
+  } else {
+    const { state, getValue } = doodle.config();
+    restartDoodle(getValue(initialState || state));
+    return initialState || state;
+  }
 }
-
--- main.js
+++ main.js
@@ -1,32 +1,26 @@
-import { html, render } from 'https://unpkg.com/lit-html?module'
-import { mountDoodle } from './doodle/doodle.js'
+import { html, render } from "https://unpkg.com/lit-html?module";
+import { mountDoodle } from "./doodle/doodle.js";
 
-const doodles = [
-	'tree',
-    'canyon',
-	'fracture',
-	'wave',
-	'coast',
-	'growth',
-]
+const doodles = ["tree", "canyon", "fracture", "wave", "coast", "growth"];
 
 const template = html`
-	${doodles.map((name) => html`
-		<div class="doodle">
-			<a id=${`link-${name}`} href=${`./doodle/?doodle=${name}`}>
-				<h3>${name}</h3>
-				<canvas id=${`canvas-${name}`}></canvas>
-			</a>
-		</div>
-	`)}
-`
+  ${doodles.map(
+    (name) => html`
+      <div class="doodle">
+        <a id=${`link-${name}`} href=${`doodle/?doodle=${name}`}>
+          <h3>${name}</h3>
+          <canvas id=${`canvas-${name}`}></canvas>
+        </a>
+      </div>
+    `,
+  )}
+`;
 
-render(template, document.getElementById('doodles'))
+render(template, document.getElementById("doodles"));
 
 for (const name of doodles) {
-	const canvas = document.getElementById(`canvas-${name}`)
-	mountDoodle(name, canvas).then((link) => {
-		document.getElementById(`link-${name}`).href = link
-	})
+  const canvas = document.getElementById(`canvas-${name}`);
+  mountDoodle(name, canvas).then((link) => {
+    document.getElementById(`link-${name}`).href = link;
+  });
 }
-
